731386adbba3999e4151ee276e63cddd8177acb5
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-07 22:27:45 +0600

    Service Page Complete

--- client/src/services/Services.jsx
+++ client/src/services/Services.jsx
@@ -2,23 +2,34 @@ import React from "react";
 import "./services.css";
 import { Col, Container, Row } from "reactstrap";
 import { motion } from "framer-motion";
+import serviceData from "./../assets/data/serviceData.js";
 
 const Services = () => {
   return (
     <section className="services">
       <Container>
         <Row>
-          <Col lg="3" md="4">
-            <div className="serviceItems">
-              <span>
-                <i className="ri-truck-line"></i>
-              </span>
-              <div>
-                <h3>Free Shipping</h3>
-                <p>Upto 10k Purchases. EMI not Included.</p>
-              </div>
-            </div>
-          </Col>
+          {serviceData.map((item, i) => (
+            <Col lg="3" md="4" key={i}>
+              <motion.div
+                whileHover={{
+                  scale: "1.1",
+                }}
+                className="serviceItems"
+                style={{
+                  background: `${item.bg}`,
+                }}
+              >
+                <span>
+                  <i className={item.icon}></i>
+                </span>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.subtitle}</p>
+                </div>
+              </motion.div>
+            </Col>
+          ))}
         </Row>
       </Container>
     </section>
