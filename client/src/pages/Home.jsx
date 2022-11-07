82110e560fcccdb15e8f608746f5948ae321fc26
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-07 12:52:21 +0600

    Working with home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-import { Container, Row } from "reactstrap";
+import "./../styles/home.css";
+import { Container, Row, Col } from "reactstrap";
 import Helmate from "../components/helmate/Helmate";
+import HeroImg from "./../assets/images/hero-img.png";
 
 const Home = () => {
+  const year = new Date().getFullYear();
+
   return (
     <>
       <Helmate title={"Home"}>
         <section className="heroSection">
           <Container>
-            <Row></Row>
+            <Row>
+              {/* Hero Content */}
+              <Col lg="6" md="6">
+                <div className="heroContent">
+                  <p className="heroSubTitle">Trending in {year}</p>
+                  <h2>Create Minimalist and Modern</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Harum exercitationem quos laudantium, minus necessitatibus
+                    quaerat praesentium eius ex, architecto magnam cumque! Cum
+                    deleniti libero aspernatur provident reprehenderit
+                    exercitationem, accusamus cumque.
+                  </p>
+                  <button className="buyBtn">Buy Now</button>
+                </div>
+              </Col>
+
+              {/* Hero Image */}
+              <Col lg="6" md="6">
+                <div className="heroImg">
+                  <img src={HeroImg} alt="hero/img" />
+                </div>
+              </Col>
+            </Row>
           </Container>
         </section>
       </Helmate>
