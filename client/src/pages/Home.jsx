59af5a6d78a50d0550cae8a6ac4e7090ab4043dd
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-07 11:31:30 +0600

    Working with Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Row } from "reactstrap";
+import Helmate from "../components/helmate/Helmate";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Helmate title={"Home"}>
+        <section className="heroSection">
+          <Container>
+            <Row></Row>
+          </Container>
+        </section>
+      </Helmate>
+    </>
+  );
 };
 
 export default Home;
