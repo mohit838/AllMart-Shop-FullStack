3ece0aafc91b4ff213495e3eb8c46a47b7a93d14
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-08 12:16:19 +0600

    Product Cart Wofrking

--- client/src/components/UI/ProductCard.jsx
+++ client/src/components/UI/ProductCard.jsx
@@ -10,11 +10,15 @@ const ProductCard = () => {
       <Col lg="3" md="4">
         <div className="productItems">
           <div className="productsImg">
-            <img src={ProductImg} alt="product/img" />
+            <motion.img
+              whileHover={{ scale: "0.9" }}
+              src={ProductImg}
+              alt="product/img"
+            />
           </div>
           <div className="p-2 productInfo">
             <h3 className="productsName">Modern Arm Chair</h3>
-            <span className="text-center d-block">Chair</span>
+            <span>Chair</span>
           </div>
           <div className="productCardBottom d-flex align-items-center justify-content-between p-2">
             <span className="price">$123</span>
